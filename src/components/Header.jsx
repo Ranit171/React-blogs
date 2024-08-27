1035e19f00d8b3d2e9ca8649707b2ff0d299333e
Ranit Basak <your-username@users.noreply.github.com>
2024-08-27 21:03:01 +0530

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="border shadow-md py-2 w-full sticky top-0 ">
       <header className="text-center ">
-        <h1 className="text-2xl font-bold">CODEHELP BLOGS</h1>
+        <h1 className="text-2xl font-bold">RANIT BLOGS</h1>
       </header>
     </div>
   );
